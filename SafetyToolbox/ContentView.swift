1a210bfe32599266694db898b7b8efe9fe00569f
hpzynha <larissa.nogueira.rocha@gmail.com>
2024-03-17 16:14:55 +1030

    Initial Commit

---
+++ SafetyToolbox/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SafetyToolbox
+//
+//  Created by Larissa Nogueira da Rocha on 17/3/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SafetyToolbox/SafetyToolboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafetyToolboxApp.swift
+//  SafetyToolbox
+//
+//  Created by Larissa Nogueira da Rocha on 17/3/2024.
+//
+
+import SwiftUI
+
+@main
+struct SafetyToolboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
